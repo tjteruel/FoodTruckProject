3676947e853b355052fed9f51d54c5f6a9df2cd4
Tim Teruel <timteruel@Administrators-MacBook-Pro.local>
2020-02-02 10:42:13 -0700

    create trucks, truck averages updated

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -2,4 +2,59 @@ package com.skilldistillery.foodtrucks;
 
 public class FoodTruck {
 
+	// F I E L D S
+	private static int truckId = 1;
+	private int individualTruckId;
+	private String truckName;
+	private String truckFood;
+	private int truckRating;
+
+	// M E T H O D S
+	public int getIndividualTruckId() {
+		return individualTruckId;
+	}
+
+	public void setIndividualTruckId(int individualTruckId) {
+		this.individualTruckId = individualTruckId;
+	}
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public String getTruckFood() {
+		return truckFood;
+	}
+
+	public void setTruckFood(String truckFood) {
+		this.truckFood = truckFood;
+	}
+
+	public int getTruckRating() {
+		return truckRating;
+	}
+
+	public void setTruckRating(int truckRating) {
+		this.truckRating = truckRating;
+	}
+
+	public FoodTruck() {
+	}
+
+	public FoodTruck(String truckName, String truckFood, int truckRating) {
+		this.individualTruckId = truckId;
+		this.truckName = truckName;
+		this.truckFood = truckFood;
+		this.truckRating = truckRating;
+		truckId++;
+	}
+
+	public String toString() {
+		return "[Truck ID: " + individualTruckId + ", name: " + truckName + ", food: " + truckFood + ", rating: "
+				+ truckRating + "]";
+	}
 }
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -1,10 +1,105 @@
 package com.skilldistillery.foodtrucks;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// F I E L D S
+	private FoodTruck[] foodTrucks = new FoodTruck[5];
+	Scanner kb = new Scanner(System.in);
 
+	// M E T H O D S
+	public static void main(String[] args) {
+
+		FoodTruckApp FTA = new FoodTruckApp();
+		String truckName;
+		String truckFood;
+		String truckRating;
+
+		FTA.createTrucks();
+		//ftCreate.displayTruckInfo();
+		// FTA.printTrucks();
 	}
 
+	public void createTrucks() {
+		for (int i = 1; i <= 5; i++) {
+			FoodTruck ftCreate = new FoodTruck();
+
+			System.out.print("Enter name of a food truck: ");
+			String name = kb.next();
+			ftCreate.setTruckName(name);
+			if (name.equalsIgnoreCase("quit")) {
+				break;
+			}
+
+			System.out.print("Enter the type of food: ");
+			String food = kb.next();
+			ftCreate.setTruckFood(food);
+			if (food.equalsIgnoreCase("quit")) {
+				break;
+			}
+
+			System.out.print("Enter the trucks rating (1-10): ");
+			int rating = kb.nextInt();
+			ftCreate.setTruckRating(rating);
+			if ( rating < 1 || rating > 10 ) {
+				System.out.print("Please rate trucks on a scale of 1 through 10. \n\tWhat is your rating:  ");
+				rating = kb.nextInt();
+				
+			}
+		}
+	}
+
+	public void printTrucksList() {
+		for (FoodTruck foodTruckList : foodTrucks) {
+			if (foodTruckList != null) {
+				System.out.println(foodTruckList);
+			} else {
+				continue;
+			}
+		}
+	}
+
+//			
+	public FoodTruck highestRated() {
+		FoodTruck highestRated = null;
+		int highestRating = Integer.MIN_VALUE;
+		for (FoodTruck foodTruckRating: foodTrucks) {
+			if (foodTruckRating == null) {
+				break;
+			}
+			if (foodTruckRating != null) {
+				if (foodTruckRating.getTruckRating() > highestRating) {
+					highestRated = foodTruckRating;
+				}
+			}
+		}
+		return highestRated;
+	}
+	
+	public double averageTruckRating(FoodTruck[] foodTruckRater) {
+		double truckRating = 0.0;
+		int counter = 0;
+		for (int i = 0; i < foodTruckRater.length; i++) {
+			if (foodTruckRater[i] != null) {
+				counter++;
+				truckRating += foodTruckRater[i].getTruckRating();
+			}
+			if (foodTruckRater[i] == null) {
+				break;
+			}
+		}
+		truckRating = truckRating / counter;
+		return truckRating;
+	}
+	
 }
+
+//			FoodTruck truckInfo = new FoodTruck();
+//				truckInfo.truckName = truckName;
+//				truckInfo.truckFood = truckFood;
+//				truckInfo.truckRating = truckRating;
+//				
+//		
+//			}
+//		}
